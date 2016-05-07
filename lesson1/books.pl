597a429bde9ac0325b027eec37fda710d23ea808
Piotr Dawidiuk <piotr.dawidiuk@gmail.com>
2016-05-07 15:30:58 +0200

    prolog - lesson1

---
+++ lesson1/books.pl
@@ -0,0 +1,13 @@
+book(theAmbasaddor).
+book(honourOfTheGrave).
+book(deathsMessenger).
+book(deathsLegacy).
+
+writter(mcNeill).
+writter(laws).
+writter(mitchell).
+
+author(mcNeill, theAmbasaddor).
+author(laws, honourOfTheGrave).
+author(mitchell, deathsMessenger).
+author(mitchell, deathsLegacy).
---
+++ lesson1/musicians.pl
@@ -0,0 +1,8 @@
+musician_instrument(fripp, guitar).
+musician_instrument(waters, guitar).
+musician_instrument(waters, clarinet).
+musician_instrument(ulrich, drumKit).
+
+musician_genre(gripp, rock).
+musician_genre(waters, rock).
+musician_genre(ulrich, metal).
